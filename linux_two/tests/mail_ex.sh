86a550dcc4410bc1ebc7234622400e7fa22dc53e
vanansa <saravananr6332@gmail.com>
2021-01-01 16:49:11 +0530

    tested inner function

--- linux_two/tests/mail_ex.sh
+++ linux_two/tests/mail_ex.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-Recipient="saravananr6332@gmail.com"
-Subject="Greeting"
-Message="Welcome to our site"
-`mail -s $Subject $Recipient <<< $Message`
+# Recipient="saravananr6332@gmail.com"
+# Subject="Greeting"
+# Message="Welcome to our site"
+# `mail -s $Subject $Recipient <<< $Message`
+
+# inner funciton testing
+
+bar() {
+    export B=something
+    echo B=$B
+    user='sara'
+    frombar(){
+        echo "$1 im the inside function who named frombar and i have values of bar too and it's $B"
+    }
+    frombar $user
+}
+
+bar
+echo B=$B
+
+
+
+
+
+
+
+
+
+
