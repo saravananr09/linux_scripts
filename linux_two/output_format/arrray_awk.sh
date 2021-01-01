f4b2c07068c832117df6c840885a61b7bb98ad5d
vanansa <saravananr6332@gmail.com>
2021-01-01 18:02:06 +0530

    priv_wise printing

--- linux_two/output_format/arrray_awk.sh
+++ linux_two/output_format/arrray_awk.sh
@@ -5,11 +5,22 @@
 # temp=$IFS  
 # IFS=$'|'
 # A=( $( ls -1p ) )
-A=("Mysql" "MariaDB" "Mongo" "msSQL" "Mysql" "MariaDB" "Mongo" "msSQL")
-
+# A=("Mysql" "MariaDB" "Mongo" "msSQL" "Mysql" "MariaDB" "Mongo" "msSQL")
+A=(Mysql MariaDB msSQL PSQL)
+B=(Firebase Mongo SimpleDB Hbase Cassandra )
 # IFS=$temp
 
-awk -v var="${A[*]}" 'BEGIN{
+read privs
+if [ $privs == 'A' ]
+then
+    # echo "A"
+    privs=(${A[@]})
+else
+    # echo "B"
+    privs=(${B[@]})
+fi
+
+awk -v var="${privs[*]}" 'BEGIN{
     split(var,list," "); for (i=1;i<=length(list);i++) 
 
     printf ("\033[1;35m %s \n" ,list[i] ) 
