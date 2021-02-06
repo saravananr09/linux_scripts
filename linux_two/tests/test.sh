51866bed904db18dcd0ec90867f1858298774b88
vanansa <saravananr6332@gmail.com>
2021-02-06 13:27:44 +0530

    tested vars

--- linux_two/tests/test.sh
+++ linux_two/tests/test.sh
@@ -1,40 +1,163 @@
 #!/bin/bash
 #  getting user input and storing it in with array
 # set -x
- declare -A config
- declare -A assigning_configs
+# echo "************************MYSQL-starting @@ "$$" ***************************"
+# # $(watch 'w') &
+# $(sleep 333)
+# trap ' ' 2
+# jobs=$(jobs -l)
+# echo $("$jobs"  )
 
-mysql_vars=(port server-id datadir socket)
-defining_configs()
-{
-	for var in ${!mysql_vars[@]}
-	do
-		read -p "Enter the  ${mysql_vars[$var]} : " value
-		config+=([${mysql_vars[$var]}]="$value") 
-		# echo ${config[$var]};
+# echo "***************************************************"
+# echo "************************Script Ended***************************"
+# exit
+
+declare -A config
+ declare -A collected_configs
+ declare -a mysql_vars
+mysql_vars=(port server-id)
+set -x
+# read_config(){
+
+# 	# read 
+# }
+run_scr(){
+	local var_name=($@)
+	echo "${var_name[@]}"
+	for i in ${!var_name[@]}
+	do	
+		echo "$i ... ${var_name[$i]}"
 	done
 }
-defining_configs
+
+run_scr ${mysql_vars[@]}
+
+
+
+# defining_configs(){
+# 	read -p "Enter the $var  ${mysql_vars[$var]} : " value
+# 	config+=([${mysql_vars[$var]}]="$value") 
+# }
+# run_scr(){
+#     local var;
+# 	last_idx=0
+# 	for var in ${!mysql_vars[@]}
+# 	do
+# 		defining_configs
+#         for key in "${!config[@]}"
+#         do
+# 	    if [ -n "${config[$key]}" ];then
+# 		    # echo "$key ===> ${config[$key]}"
+# 			echo "assigning_configs ....  $key ==>  ${config[$key]} "
+# 		    collected_configs+=(["$key"]=${config[$key]})
+# 	    else
+# 		    echo "$key has no values"
+# 		    unset config[$key]
+# 	    fi
+# 		# ((last_idx=${#mysql_vars[@]} - 1))
+#         done
+# 	done
+
+# }
+# run_scr
+
+
+
+exit
+
+ 
+ 
+ declare -A config
+ declare -A assigning_configs
+ declare -a mysql_vars
+echo "***************************************************"
+echo "Assigning variables for mysql"
+echo "***************************************************"
+# vars datadir socket
+mysql_vars=(port server-id)
+# defining_configs(){
+# 	read -p "Enter the $var  ${mysql_vars[$var]} : " value
+# 	config+=([${mysql_vars[$var]}]="$value") 
+# 	# echo ${config[$var]};
+# }
+
+
+# run_scr(){
+# 	for var in ${!mysql_vars[@]}
+# 	do
+# 		defining_configs
+# 	done
+
+# }
+
+# 
+
+defining_configs () {
+	if [[ "$#" -gt 0 ]]
+	then
+		echo "argu'ss"
+		for argument in "$#"
+		do
+		read -p "Enter the $var  ${mysql_vars[$((argument - 1))]} : " value
+		config+=([${mysql_vars[$var]}]="$value")
+		done
+	else
+		echo "no -argus"
+		read -p "Enter the $var  ${mysql_vars[$var]} : " value
+		config+=([${mysql_vars[$var]}]="$value") 
+	fi
+	
+	# echo ${config[$var]};
+}
+run_scr () {
+	for var in ${!mysql_vars[@]}
+	do
+		defining_configs "${mysql_vars[$var]}"
+	done
+
+}
+# 
+# defining_configs
+run_scr
 
 for key in "${!config[@]}"
 do
 	if [ -n "${config[$key]}" ];then
-		echo "$key ===> ${config[$key]}"
+		# echo "$key ===> ${config[$key]}"
 		assigning_configs+=(["$key"]=${config[$key]})
 	else
+	# set -x
 		echo "$key has no values"
+		second_run+=($key)
+		unset mysql_vars[*]
+		sleep 1
+		echo "${mysql_vars[*]}"
+		mysql_vars+=($key)
+		defining_configs ${second_run[*]}
 		unset config[$key]
 	fi
 done
 
-set -x
-echo "
-	printing final configs
-	${!assigning_configs[@]}'=='${assigning_configs[@]}
-"
+# re_assign(){
 
+# }
+
+
+
+# set -x
+echo "***************************************************"
+echo "Printing values from collected-variables"
+echo "***************************************************"
+# echo "printing final configs ${!assigning_configs[@]}'=='${assigning_configs[@]}"
+for output in "${!assigning_configs[@]}"
+do
+	echo " $output=${assigning_configs[$output]}";
+done 
+
+exit 1	
+echo "********************* END ******************************"
 # echo ${config[@]}
-sleep 10
+sleep 1
 
 
 
@@ -44,6 +167,18 @@ sleep 10
 
 
 
+
+
+# # echo " ${!mysql_vars[@]}    ...    ${#mysql_vars[@]}"
+# i=0;
+# for i in ${!mysql_vars[@]}
+# do	
+# 	echo "$i";
+# 	let "i=i-1";
+# 	echo "$i"
+# 	# i=$(($i-1))
+#     # echo "$i .... ${mysql_vars[$i]}"
+# done
 
 
 # read -p "Enter the basedir : " basedir
@@ -76,12 +211,12 @@ sleep 10
 # else 
 # 	echo "not founded";
 # fi
-
+#
 set -x
 
 echo "${!config[@]}"
 
-sleep 10
+# sleep 10
 
 
 # if [ -n "$string" ]
@@ -94,7 +229,16 @@ sleep 10
 
 
 
-
+# run(){
+	# if [[ "$#" > 0 ]]
+	# then
+	# 	echo "argu'ss"
+	# else
+	# 	echo "no -argus"
+	# fi
+# }
+# run
+# exit
 
 
 
