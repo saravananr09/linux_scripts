664a2ed98ee2f8fa9a8e2ceb7806d6f2e5e8e9f1
vanansa <saravananr6332@gmail.com>
2021-02-12 19:27:33 +0530

    added func on def var

--- linux_two/tests/defining_variables.sh
+++ linux_two/tests/defining_variables.sh
@@ -1,69 +1,67 @@
 #!/bin/bash
 #  getting user input and storing it in with array
-# set -x
+#  set -x
 
  declare -A config
  declare -A assigning_configs
  declare -a mysql_vars
+ declare -a unassigned_mysql_vars
+
 echo "***************************************************"
 echo "Assigning variables for mysql"
 echo "***************************************************"
-# vars datadir socket
+# vars datadir socket  /// (( i=0; i<="${#split_var[@]}"; i++))
 mysql_vars=(port server-id)
 defining_configs(){
-	read -p "Enter the $var  ${mysql_vars[$var]} : " value
-	config+=([${mysql_vars[$var]}]="$value") 
+	declare -a split_var
+	split_var=($@)
+	echo "from split_var :: ${split_var[@]}"
+	for enter in ${!split_var[@]}
+	do
+	read -p "Enter($enter) the $var_name  ${split_var[$var_name]} : " value
+	config+=([${split_var[$var_name]}]="$value") 
+	done
 	# echo ${config[$var]};
 }
 
 
 run_scr(){
-    local var
-	for var in ${!mysql_vars[@]}
+    local var_name
+	for var_name in ${!mysql_vars[@]}
 	do
-		defining_configs
+		defining_configs ${mysql_vars[@]}
 	done
-
-}
-
-run_scr
-
-for key in "${!config[@]}"
-do
+	for key in "${!config[@]}"
+	do
 	if [ -n "${config[$key]}" ];then
 		# echo "$key ===> ${config[$key]}"
 		assigning_configs+=(["$key"]=${config[$key]})
 	else
 	# set -x
-		echo "$key has no values" && second_run+=($key) 
-		# unset mysql_vars[*]
-		# sleep 1
-		# echo "${mysql_vars[*]}"
-        
-		# mysql_vars+=($key)
-		# defining_configs ${second_run[*]}
+		echo "$key has no values"  
 		unset config[$key]
-        sleep 1
-        echo "${config[@]}"
-        sleep 1
-	fi
-    
-done
-    echo "following var need to re-assign ( ${second_run[*]} )"
-
-# re-assign
-re-defining_configs(){
-    
-	read -p "Enter the $var  ${mysql_vars[$var]} : " value
-	config+=([${mysql_vars[$var]}]="$value") 
-	# echo ${config[$var]};
+		unassigned_mysql_vars+=($key)
+	    sleep 1
+        # echo "${config[@]}"
+	fi    
+	done
 }
+    #  echo "following var need to re-assign ( ${second_run[*]} )"
 
+run_scr
 
-exit
-# re_assign(){
-
-# }
+printf "\n"
+# re-assign
+#  set -x
+if [[ "${#unassigned_mysql_vars[@]}" -gt "${#_mysql_vars[@]}" ]]
+then 
+	echo -e "need to re-assign ! \n"
+	echo "inside if of ${unassigned_mysql_vars[@]}"
+	sleep 1
+	defining_configs ${unassigned_mysql_vars[@]}
+else	
+	echo -e "Vars has assigned ! \n "
+fi
 
 
 
@@ -77,10 +75,11 @@ do
 	echo " $output=${assigning_configs[$output]}";
 done 
 
-exit 1	
+# exit 1	
 echo "********************* END ******************************"
 # echo ${config[@]}
 sleep 1
+
 
 
 
